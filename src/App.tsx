2be17ccc7385373a5deb46a3bbc0982ea23f7c34
LUCAS NOGUEIRA <102195514+ZEROONZE@users.noreply.github.com>
2022-07-04 06:32:15 -0300

    Laout 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,23 @@ import Content from './components/Content';
 import { Grid } from './components/Layout/styles';
 import MainHeader from './components/MainHeader';
 import GlobalStyles from './styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import Layout from './components/Layout';
+import dark from './styles/themes/dark';
 
-
+ 
 const App: React.FC = () => { 
       
       return ( 
-      <Grid>
-    <MainHeader />
-    <Aside />
-    <Content />
+        <ThemeProvider theme={dark}>
+    
+    
+    
+   <Layout />
     <GlobalStyles />
 
-    </Grid>
+
+    </ThemeProvider>
   );
 }
 
